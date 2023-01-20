30cb89b5184525cf9ea5d105d0885140e24c0060
dairinka <104695300+dairinka@users.noreply.github.com>
2023-01-20 14:53:33 +0200

    feat: add error message when server turn off

---
+++ src/ts/error/showError.ts
@@ -0,0 +1,17 @@
+export function showError(errorMessage: string): void {
+  const errorFragment = <DocumentFragment>document.createDocumentFragment();
+  const errorWrapEl = <HTMLDivElement>document.createElement("div");
+  const errorMessageEl = <HTMLParagraphElement>document.createElement("p");
+  errorWrapEl.className = "error-wrap";
+  errorMessageEl.className = "error-message";
+  errorMessageEl.textContent = errorMessage;
+  errorWrapEl.append(errorMessageEl);
+  errorFragment.append(errorWrapEl);
+  document.body.append(errorFragment);
+}
+
+export function clearError(): void {
+  const errorWrapEl = <HTMLElement>document.querySelector(".error-wrap");
+  errorWrapEl.remove();
+  console.log("delete error");
+}
